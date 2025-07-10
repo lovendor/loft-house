7b89f20255f814b7204958c0e30a5409bd3f3c00
i lvme <doszhanbeken001@google.com>
2025-07-10 14:10:24 +0500

    Обновил код

--- src/app/store/auth/auth.reducer.ts
+++ src/app/store/auth/auth.reducer.ts
@@ -6,14 +6,26 @@ export interface AuthState {
 }
 
 export const initialAuthState: AuthState = {
-  user: null
+  user: typeof window !== 'undefined' && localStorage.getItem('user')
+    ? JSON.parse(localStorage.getItem('user')!)
+    : null
 };
 
 export const authReducer = createReducer(
   initialAuthState,
-  on(login, (state, { username }) => ({
-    ...state,
-    user: { username }
-  })),
-  on(logout, () => initialAuthState)
+
+  on(login, (state, { username }) => {
+    const user = { username };
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('user', JSON.stringify(user));
+    }
+    return { ...state, user };
+  }),
+
+  on(logout, (state) => {
+    if (typeof window !== 'undefined') {
+      localStorage.removeItem('user');
+    }
+    return { ...state, user: null };
+  })
 );
